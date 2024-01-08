02a925c4d093013a6bca045ef1ecf6d10fc17a4d
fullstack290623 <fullstack290623@gmail.com>
2024-01-08 21:40:14 +0200

    Update rec_q.java

--- java/rec_q.java
+++ java/rec_q.java
@@ -6,6 +6,12 @@ public class rec_q {
         if (number2 <= 0)
             return number1;
         return add_rec(number1 + 1, number2 - 1);
+        // add(8, 4)
+        //   return add(9, 3)
+        //     return add(10, 2)
+        //       return add(11, 1)
+        //         return add(12, 0)
+
     }
 
     private static void print_countdown(int number) {
@@ -15,9 +21,25 @@ public class rec_q {
         print_countdown(number);
     }
 
+    public static int mul_rec(int a, int b) {
+        if (a == 0 || b == 0) {
+            return 0;
+        }
+        return a + mul_rec(a, b - 1);
+        // mul(8, 4)
+        // 8 + mul(8, 3) = 32
+        //     8 + mul(8, 2) = 24
+        //          8 + mul(8, 1) = 16
+        //              8 + mul (8, 0) = 8
+        //                       0
+    }
+
     public static void main(String[] args) {
         int number = 5;
         print_countdown(number);
+
+        var x = mul_rec(8, 4);
+        System.out.println(x);
 
         System.out.println(add_rec(5, 3));
     }
@@ -27,7 +49,7 @@ public class rec_q {
     // print_files('c:\games\fortnite-12')
 
     // 1. create recursive function that calculates a - b
-    // 1. create recursive function that calculates a * b
+    // 2. create recursive function that calculates a * b
 
 
 
